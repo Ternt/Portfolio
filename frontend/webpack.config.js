4e2b6982d176da2fc57feec2b56ed8b6a160624a
Thinh Pham <kyle.pham202@gmail.com>
2024-11-12 23:06:45 -0500

    react

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,36 +2,38 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 const config = {
-    context: path.resolve(__dirname, 'src'),
-    entry: {
-        app: { import: './index.js' }
-    },
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'main.js'
-    },
-    watch: true,
-    watchOptions: { ignored: /node_modules/ },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: { presets: ['@babel/preset-react'] }
-                }
-            },
-            { test: /\.(ts|tsx)$/, use: 'ts-loader' },
-        ]
-    },
-    plugins: [ new HtmlWebpackPlugin({ template: './index.html' }) ]
+	context: path.resolve(__dirname, 'src'),
+	entry: {
+		app: { import: './index.js' }
+	},
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'main.js'
+	},
+	watch: true,
+	watchOptions: { ignored: /node_modules/ },
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+			{ 
+				test: /\.(ts|tsx)$/, 
+				exclude: /node_modules/,
+				use: 'ts-loader' 
+			},
+		]
+	},
+	plugins: [ new HtmlWebpackPlugin({ template: './index.html' }) ]
 }
 
 module.exports = (env, argv) => {
-    if (argv.mode === 'development') {
-        config.devtool = 'source-map'
-    }
+	if (env.mode === 'development') {
+		config.devtool = 'source-map'
+		config.mode = env.mode;
+	}
 
-    return config;
+	return config;
 }
