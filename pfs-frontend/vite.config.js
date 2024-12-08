94f788e0c9210badd933e24163bd1df1d2d056f4
Ternt <kyle.pham202@gmail.com>
2024-12-08 00:55:52 -0500

    svelte init

--- pfs-frontend/vite.config.js
+++ pfs-frontend/vite.config.js
@@ -1,10 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-    },
-})
+	plugins: [sveltekit()]
+});
