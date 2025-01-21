088b6b60e99cb656bbabc72310f527f8618a8eeb
Ternt <kyle.pham202@gmail.com>
2025-01-21 01:12:49 -0500

    simple splittext library implementation, crappy per-letter text animation

--- src/util/splittext.js
+++
@@ -1,9 +0,0 @@
-
-// Takes in the dom element that contains text to be split
-export default function SplitText(domelement) {
-    let dom = document.querySelector(domelement); 
-    let letters = dom.textContent.trim().split('');
-    console.log(dom.body);
-
-    return {};
-}
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,14 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
+
+    let outputPath = path.resolve(__dirname, "public");
     let options = {
         mode: (env.WEBPACK_SERVE) ? 'development' : 'production',
         entry: path.resolve(__dirname, "src/index.js"),
         output: {
-            path: path.resolve(__dirname, "public"),
+            path: outputPath,
             filename: "[name].[contenthash].js",
-            publicPath: "/"
+            publicPath: "/",
+            clean: true,
         },
         module:{
             rules: [
@@ -35,6 +39,12 @@ module.exports = (env, argv) => {
             new HtmlWebpackPlugin({
                 template: path.resolve(__dirname, 'src/index.html'),
                 filename: 'index.html',
+            }),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: path.resolve(__dirname, "src/assets"), to: path.join(outputPath, 'assets') },
+                    { from: path.resolve(__dirname, "src/pages"), to: path.join(outputPath, 'pages') }
+                ]
             })
         ],
         optimization: {
@@ -54,7 +64,7 @@ module.exports = (env, argv) => {
     if (env.WEBPACK_SERVE) {
         options["devServer"] = {
             port: "8080",
-            static: path.resolve(__dirname, "public"),
+            static: outputPath,
             liveReload: true
         }
     }
